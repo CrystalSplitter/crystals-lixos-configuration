2d8d0750096aac7644711fdf8a2f2f371a2fb200
CrystalSplitter <crystal@crystalwobsite.gay>
2025-06-17 00:52:28 -0700

    Reformat hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,46 +1,61 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-partlabel/seafoam-60g-root";
-      fsType = "btrfs";
-      options = [
-        "subvol=@"
-        "noatime"
-        "nodiratime"
-        "discard"
-      ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-partlabel/seafoam-60g-root";
-      fsType = "btrfs";
-      options = [
-        "subvol=@home"
-        "noatime"
-        "nodiratime"
-        "discard"
-      ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/98BC-F460";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/7e7797b1-b6f2-47cf-ae4c-c2a5c01fe762"; }
+  fileSystems."/" = {
+    device = "/dev/disk/by-partlabel/seafoam-60g-root";
+    fsType = "btrfs";
+    options = [
+      "subvol=@"
+      "noatime"
+      "nodiratime"
+      "discard"
     ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-partlabel/seafoam-60g-root";
+    fsType = "btrfs";
+    options = [
+      "subvol=@home"
+      "noatime"
+      "nodiratime"
+      "discard"
+    ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/98BC-F460";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/7e7797b1-b6f2-47cf-ae4c-c2a5c01fe762"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -52,7 +67,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  
+
   services.smartd = {
     enable = true;
     devices = [
